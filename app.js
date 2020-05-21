df170a98a5c0f426ee9c470cc3c76d3ce8ec90c8
Nancy Tochi O <nancytochi24@gmail.com>
2020-05-21 14:43:18 -0400

    Update 7 RESTful Routes & functionality

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const bodyParser = require("body-parser"),
+expressSanitizer = require("express-sanitizer"),
+methodOverride   = require("method-override"),
 mongoose         = require("mongoose"),
 express          = require("express"),
-app              = express()
+app              = express(),
+port             = process.env.PORT || 5500;
 
 
 //App Config
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+mongoose.set('useFindAndModify', false);
 mongoose.connect("mongodb://localhost:27017/vernal_blog", { useNewUrlParser: true, useUnifiedTopology: true });
 
 
@@ -19,10 +25,13 @@ const blogSchema = new mongoose.Schema({
     body: String,
     created: {type: Date, default: Date.now}
 });
+
 //Mongoose Config: Compile Schema into a model to access methods
 const Blog = mongoose.model("Blog", blogSchema);
 
 
+
+//Create first/test database
 // Blog.create(
 // {
 // 	title: "Test Blog",
@@ -41,7 +50,7 @@ const Blog = mongoose.model("Blog", blogSchema);
 
 
 //RESTful Routes
-//INDEX- display all blogs
+//INDEX- list all blogs
 app.get("/", function(req,res){
     res.redirect("/blogs")
 });
@@ -56,16 +65,68 @@ app.get("/blogs", function(req, res){
 	});
 });
 
+//NEW- show new blog form
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+
+//CREATE- create a new blog, then redirect
+app.post("/blogs", function(req, res){
+	//Get data from form & save to database
+	Blog.create(req.body.blog, function (err, newBlog) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect("/blogs");
+		}
+	});
+});
+
+//SHOW- show info about a specific blog
+app.get("/blogs/:id", function (req, res){
+	Blog.findById(req.params.id, function(err, specificBlog){
+		if(err){
+			res.redirect("/blogs")
+		} else {
+			res.render("show", {blog: specificBlog});
+		}
+	});
+});
+
+//EDIT- show edit form for one blog
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, editBlog){
+		if(err){
+			res.redirect("/blogs")
+		} else {
+			res.render("edit", {blog: editBlog});
+		}
+	});
+});
+
+//UPDATE- update particular blog, then redirect
+app.put("/blogs/:id", function (req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updateBlog){
+		if(err){
+			res.redirect("/blogs/:id/edit")
+		} else {
+			res.redirect("/blogs/" + req.params.id)
+		}
+	});
+});
+
+//DESTROY- delete a particular blog, then redirect
+app.delete("/blogs/:id", function (req, res){
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs")
+		} else {
+			res.redirect("/blogs")
+		}
+	});
+});
 
 
-
-
-
-
-
-
-
-const port = process.env.PORT || 5500;
 
 app.listen(port, function(){
 	console.log("Vernal Blog Has Started!");
